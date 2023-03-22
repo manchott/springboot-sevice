786ef6461c0b88d05ebf73037f9ed674fdeef0bc
박서영 <manchott@naver.com>
2023-03-22 15:23:07 +0900

    Initial commit

---
+++ src/main/java/com/manchott/springbootsevice/SpringbootSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.manchott.springbootsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manchott/springbootsevice/SpringbootSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manchott.springbootsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
